5c7737bd7f9c2ef54bf8be98e7d58213982e6dd4
Oriana Graterol <oggs22@gmail.com>
2021-08-07 17:26:25 -0400

    added header

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import usePixels from '../hooks/usePixels'
 import metamaskLogo from '../public/images/metamask-logo.png'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
 
@@ -11,18 +12,15 @@ export default function Home() {
   } = usePixels()
 
   return (
-    <div className="flex h-screen">
-      <div className="m-auto">
-        <div className="p-6 max-w-sm mx-auto bg-blue-500 rounded-xl shadow-md flex items-center space-x-4">
-          <div className="flex-shrink-0">
-            <Image src={metamaskLogo} alt="ChitChat Logo" width={72} height={72} className="h-12 w-12" />
-          </div>
-          <div>
-            <div className="text-xl font-medium text-white">Metamask</div>
-            <p className="text-white">Connect</p>
-          </div>
-        </div>
-      </div>
+    <div>
+      <Head>
+        <title>Million Pixels</title>
+        <link rel="icon" href="/favicon.ico"></link>
+      </Head>
+      <Navbar/>
+      <main>
+        <h1>Todo App</h1>
+      </main>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue'   : '#6E01FE',
+        'medium-blue'  : '#15AEFC',
+        'dark-blue'    : '#3482D8'
+      }
+    },
   },
   variants: {
     extend: {},
